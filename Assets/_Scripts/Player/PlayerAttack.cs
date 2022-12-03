63d9ccede65e0b8cff09f1b0f3498dac2e3c7e29
Ramiro Esaú Ramírez Alonzo <ramirocell10@hotmail.com>
2022-12-02 22:43:48 -0600

    Added base attack script

--- Assets/_Scripts/Player/PlayerAttack.cs
+++ Assets/_Scripts/Player/PlayerAttack.cs
@@ -1,18 +1,57 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private PlayerInput _input;
+    private InputAction _attackAction;
+    private InputAction _attackUpAction;
+    private InputAction _attackDownAction;
+
+    public Action AttackDelegate;
+    public Action AttackUpDelegate;
+    public Action AttackDownDelegate;
+
+    private void Awake()
     {
-        
+        _input = GetComponent<PlayerInput>();
+
+        _attackAction = _input.actions["Attack"];
+        _attackUpAction = _input.actions["AttackUp"];
+        _attackDownAction = _input.actions["AttackDown"];
+    }
+    private void OnEnable()
+    {
+        _attackAction.performed += Attack;
+        _attackUpAction.performed += AttackUp;
+        _attackDownAction.performed += AttackDown;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        _attackAction.performed -= Attack;
+        _attackUpAction.performed -= AttackUp;
+        _attackDownAction.performed -= AttackDown;
+    }
+
+    private void Attack(InputAction.CallbackContext ctx)
+    {
+        if (_attackUpAction.WasPerformedThisFrame() || _attackDownAction.WasPerformedThisFrame()) return;
+
+        Debug.Log("Basic");
+        AttackDelegate?.Invoke();
+    }
+    private void AttackUp(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("Up");
+        AttackUpDelegate?.Invoke();
+    }
+    private void AttackDown(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("Down");
+        AttackDownDelegate?.Invoke();
     }
 }
